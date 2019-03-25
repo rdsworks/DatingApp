8392e9ae1430baba739de9580bb77099031c1839
Dan <dan.stankewich@outlook.com>
2019-03-25 17:52:01 -0500

    End of section 10

--- DatingApp.API/Helpers/AutoMapperProfiles.cs
+++ DatingApp.API/Helpers/AutoMapperProfiles.cs
@@ -24,6 +24,7 @@ namespace DatingApp.API.Helpers
                     opt => opt.ResolveUsing(d => d.DateOfBirth.CalculateAge())
                 );
             CreateMap<Photo, PhotosForDetailedDto>();
+            CreateMap<UserForUpdateDto, User>();
         }
     }
 }
